d5d19b7b47dfa6caa236445fb15d00394a1ac763
Eager <eagerbeaver0353@gmail.com>
2023-10-30 14:50:53 +0000

    added logging feature

--- services/chain.py
+++ services/chain.py
@@ -18,27 +18,43 @@ from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
 max_tokens = 32768
 
+
 def configure_conversational_retrieval_chain():
-    
     # LLM
-    llm = ChatOpenAI(streaming=True, callbacks=[StreamingStdOutCallbackHandler()], temperature=0, verbose=True)
-    memory = ConversationSummaryMemory(llm=llm, memory_key="chat_history",return_messages=True)
-    
+    llm = ChatOpenAI(
+        streaming=True,
+        callbacks=[StreamingStdOutCallbackHandler()],
+        temperature=0,
+        verbose=True,
+    )
+    memory = ConversationSummaryMemory(
+        llm=llm, memory_key="chat_history", return_messages=True
+    )
+
     CHROMA_DB_DIRECTORY = os.environ.get("CHROMA_DB_DIRECTORY")
-    vectorstore = Chroma(persist_directory=CHROMA_DB_DIRECTORY, embedding_function=OpenAIEmbeddings())
+    vectorstore = Chroma(
+        persist_directory=CHROMA_DB_DIRECTORY, embedding_function=OpenAIEmbeddings()
+    )
 
     retriever = vectorstore.as_retriever()
     qa = ConversationalRetrievalChain.from_llm(llm, retriever=retriever, memory=memory)
     return qa
 
+
 def configure_retrieval_chain(**kwargs):
-    
     # LLM
     llm = OpenAI(verbose=True, **kwargs)
-    
-    CHROMA_DB_DIRECTORY = os.environ.get("CHROMA_DB_DIRECTORY")
-    vectorstore = Chroma(persist_directory=CHROMA_DB_DIRECTORY, embedding_function=OpenAIEmbeddings())
 
-    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=vectorstore.as_retriever(search_kwargs={'k': 1}))
+    CHROMA_DB_DIRECTORY = os.environ.get("CHROMA_DB_DIRECTORY")
+    vectorstore = Chroma(
+        persist_directory=CHROMA_DB_DIRECTORY, embedding_function=OpenAIEmbeddings()
+    )
+
+    qa = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=vectorstore.as_retriever(search_kwargs={"k": 1}),
+        chain_type_kwargs={"verbose": True},
+    )
 
     return qa
